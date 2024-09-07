1670fffb8ce7c9aebd179b173c214a88fae28296
0Pinky0 <lpinkyl@126.com>
2024-09-07 22:49:07 +0800

    Added configs for noise.

--- envs/cpp_env_base.py
+++ envs/cpp_env_base.py
@@ -51,7 +51,7 @@ class CppEnvBase(gym.Env):
             use_box_boundary: bool = True,  # 在场景外是否绘制障碍物包裹
             use_traj: bool = True,
             noise_position: Optional[float] = 1.0,
-            noise_direction: Optional[float] = 1.0,
+            noise_direction: Optional[float] = 5.0,
             noise_weed: Optional[float] = 0.1,
             map_dir: str = 'envs/maps/1-400',
     ):
