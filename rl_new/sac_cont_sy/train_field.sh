f247ee239a0748c1846e9edf51d633381149965c
starry <starry.hexinyin@gmail.com>
2025-09-15 21:49:14 +0800

    09-15 server 覆盖奖励*10 训练V4环境版本

--- rl_new/sac_cont_sy/train_field.sh
+++ rl_new/sac_cont_sy/train_field.sh
@@ -1,1 +1,2 @@
-python sac.py logger.exp_name="Field_v4_Sync-New-Code-01Try"  buffer.temp_dir="/root/rl_temp_field_train_data" logger.group_name="Field"
+python sac.py logger.exp_name="Field_v4_Sync-New-Code-01Try"  buffer.temp_dir="/root/rl_temp_field_train_data" \
+logger.group_name="Field" env.env_id: "NewPasture-v4" collector.total_frames=10_000_000
